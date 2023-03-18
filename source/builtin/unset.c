7b3a82303e39749759a1b2ca74702fe0fe28409f
burcakseven <burcakseven123@gmail.com>
2023-03-19 02:17:33 +0300

    heredoclar düzeltilmeden son commit(her pipe için heredoc eklenecek)

--- source/builtin/unset.c
+++ source/builtin/unset.c
@@ -14,9 +14,6 @@
 
 t_data	*g_data;
 
-// aklımda ki fikir get_index ile = 'in olduğu index i bulup ona göre strncmp yapıp doğrumu env'de mi değil mi ona bakmak
-// sonrasında silmek ancak 1'den çok unset verirse ????
-// çalışıyor bu yüzden 10dk da çıkan bu +30 saat uykusuz çalışmakla.
 int get_index(const char *str)
 {
     int i = 0;
@@ -28,19 +25,25 @@ int get_index(const char *str)
 
 void delete_env(const char *varname)
 {
-    int env_len = chardb_len(g_data->env);
-    char **new_env = (char **)malloc(sizeof(char *) * (env_len + 1));
-    if (new_env == NULL) {
+    int index;
+    int env_len;
+    char **new_env;
+    int j = 0;
+    int i = 0;
+
+    env_len = chardb_len(g_data->env);
+    new_env = (char **)malloc(sizeof(char *) * (env_len + 1));
+    if (new_env == NULL)
+    {
         printf("Error: Memory allocation failed.\n");
         return;
     }
-
-    int j = 0;
-    for (int i = 0; i < env_len; i++) {
-        int index = get_index(g_data->env[i]);
-        if (ft_strncmp(g_data->env[i], varname, index) != 0) {
+    while (i < env_len)
+    {
+        index = get_index(g_data->env[i]);
+        if (ft_strncmp(g_data->env[i], varname, index) != 0)
             new_env[j++] = g_data->env[i];
-        } 
+        i++;
     }
     new_env[j] = NULL;
     g_data->env = new_env;
